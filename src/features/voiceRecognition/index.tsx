0b8641887eec5faa2556c91f9cae6581fe164778
Fn-front <reservoir-station@outlook.jp>
2024-10-05 18:00:06 +0900

    add: ダミーデータ移動

--- src/features/voiceRecognition/index.tsx
+++ src/features/voiceRecognition/index.tsx
@@ -5,6 +5,7 @@ import Conversations from './components/conversations'
 import { Conversation } from "./types/conversation";
 import style from './index.module.scss'
 import VoiceField from "./components/voiceField";
+import { voiceConversation } from './constatants/data'
 
 declare global {
   interface Window {
@@ -18,19 +19,7 @@ export const VoiceRecognition = () => {
   const scrollBottomRef = useRef<HTMLDivElement>(null);
   
   const [transcript, setTranscript] = useState<string>('');
-  const [conversation, setConversation] = useState<Array<Conversation>>([
-    {id: 8747722302, text: 'コンビニ'},
-    {id: 5070858639, text: 'コメ'},
-    {id: 4800580597, text: 'さようなら'},
-    {id: 5333066174, text: 'モックやで'},
-    {id: 4629803116, text: '今日は晴れです'},
-    {id: 6650202305, text: '新しい本を読み始めたら、とても面白くて一気に進んだ。'},
-    {id: 6773904590, text: '先日訪れた美術館では、特別展が開催されていて、多くの貴重な作品を見ることができました。'},
-    {id: 5714100651, text: '電車の窓から見える田園風景が次第に山に変わり、遠くには青い海が広がる。駅に降り立つと、心地よい潮風が頬を撫で、静かな港町に旅が始まる予感がした。'},
-    {id: 2617395506, text: '古い町並みが広がる石畳の道を歩きながら、香ばしい焼きたてのパンの香りが漂う。道端には小さなカフェが並び、風に揺れる花々が美しい。旅先の空気が心を癒してくれる。'},
-    {id: 1358668873, text: '山間の小さな温泉街に降り立つと、静かな川のせせらぎと湯けむりが迎えてくれた。古い旅館の木の香りが漂い、心が落ち着く。ゆっくりと温泉に浸かりながら、日常を忘れるひとときを楽しんだ。'},
-    // {id: 3603930057, text: '朝早く起きて、まだ人の少ない静かな海辺を歩く。波の音が心地よく、砂浜にはカモメが飛び交っている。遠くの水平線には漁船が見え、太陽がゆっくりと昇り始め、金色の光が海面を照らしていた。'},
-  ]);
+  const [conversation, setConversation] = useState<Array<Conversation>>(voiceConversation);
   const [message, setMessage] = useState<string>('')
   const [isListening, setIsListening] = useState(false);
 
